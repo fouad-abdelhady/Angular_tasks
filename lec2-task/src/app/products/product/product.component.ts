336b5218de7d0f8cd01338df69ec06deb8d6b76b
krrish_dev <justbegintoday@gmail.com>
2023-07-17 20:57:06 +0300

    Fork my repo

--- lec2-task/src/app/products/product/product.component.ts
+++ lec2-task/src/app/products/product/product.component.ts
@@ -1,25 +1,28 @@
-import { Component, Injectable, Input } from "@angular/core";
-import {Product} from "../models/productModel";
+import { Component, Input } from "@angular/core";
+import { Product } from "../models/productModel";
 
-
-@Component(
-    {
-        selector: "productItem-root",
-        templateUrl: "./product.component.html",
-        styleUrls: ["./product.component.scss"]
-    }
-    )
+@Component({
+  selector: "productItem-root",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.scss"]
+})
 export class ProductItemComponent {
-    @Input() productItem: Product;
-    @Input() products!: Product[];
-    constructor(){
-        this.productItem = new Product();
-    }
+  @Input() productItem: Product;
+  @Input() products!: Product[];
+  @Input() filter!: string;
 
-    largerThan100(product:Product){
-      return product.priceProduct>100
-    }
+  constructor() {
+    this.productItem = new Product();
+  }
 
+  largerThan100(product: Product) {
+    return product.priceProduct > 100;
+  }
 
+  shouldBeViewed(product: Product): boolean {
+    if (this.filter === "All") return true;
+    if (this.filter === "Available" && product.isAvailable) return true;
+    if (this.filter === "Featured" && product.isFeatured) return true;
+    return false;
+  }
 }
-
